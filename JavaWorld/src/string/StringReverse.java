d916f013e4fe463c2abfb1b71ddc3b984bea1130
Krishna-kashu <krishnavenin8192@gmail.com>
2025-04-15 12:26:17 +0530

    working on String operations

--- JavaWorld/src/string/StringReverse.java
+++ JavaWorld/src/string/StringReverse.java
@@ -2,12 +2,20 @@ package string;
 
 public class StringReverse {
         public static void reverseSentence(String sentence) {
-            StringBuilder stringBuilder = new StringBuilder(sentence);
-            stringBuilder.reverse();
-            System.out.println("Reversed Sentence: " + stringBuilder);
+
+            String revString = "";
+            char[] arr = sentence.toCharArray();
+            int count = 0;
+            for (char ch : arr) {
+                count++;
+            }
+            for (int i = count - 1; i >= 0; i--) {
+                revString = revString + sentence.charAt(i);
+            }
+            System.out.println("Reversed Sentence: " + revString);
         }
     public static void main(String[] args) {
-        String sentence = "Hello, World!";
+        String sentence = "Bangalore is a city";
         reverseSentence(sentence);
     }
     }
