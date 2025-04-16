eefd9f9fdc239034b878580172441635f9c2ccb8
Krishna-kashu <krishnavenin8192@gmail.com>
2025-04-16 20:24:06 +0530

    work on casting

--- com.oopworkz.core/src/com/access/Door.java
+++ com.oopworkz.core/src/com/access/Door.java
@@ -1,8 +1,25 @@
 package com.access;
 
 public class Door  {
+    private int doorNo;
+    private String doorName;
+
     public Door() {
-        System.out.println("Constructor of Door");
+        System.out.println(" no-arg Constructor of Door");
+    }
+
+    public Door(String doorName, int doorNo){
+        this.doorName=doorName;
+        this.doorNo=doorNo;
+        System.out.println("String, int Constructor of Door");
+    }
+    public Door(Door door){
+        this.doorNo= door.doorNo;;
+        this.doorName= door.doorName;
+    }
+    @Override
+    public String toString(){
+        return "Door No="+doorNo+" Door Name="+doorName;
     }
 
     public void open() {
--- com.oopworkz.core/src/com/access/DoorRunner.java
+++ com.oopworkz.core/src/com/access/DoorRunner.java
@@ -13,5 +13,9 @@ public class DoorRunner {
         SmartDoor smartDoor = new SmartDoor();
         smartDoor.open();
         smartDoor.lockRemotely();
+
+        Door door2=new Door("janani",9);
+        Door door3=new Door(door2);
+        System.out.println(door3);
     }
 }
