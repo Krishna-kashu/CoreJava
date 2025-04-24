c3e26f5566f95207595947cba43b3d224838700a
Krishna-kashu <krishnavenin8192@gmail.com>
2025-04-24 15:02:19 +0530

    JDBC connection

--- oreo/src/com/oopsworkz/internal/Instagram.java
+++ oreo/src/com/oopsworkz/internal/Instagram.java
@@ -5,14 +5,13 @@ public class Instagram {
 
     public Instagram(JDBC jdbc){
         this.jdbc=jdbc;
+    }
+    public void open(){
+        System.out.println("Running open in Instagram");
         if(jdbc!=null){
             this.jdbc.save();
         }else{
             System.out.println("null");
         }
-    }
-    public void open(){
-        System.out.println("Running open in Instagram");
-
     }
 }
