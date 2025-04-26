e0b4efbc87b0f35a49e9f0a4123ef01926f7bd3c
Krishna-kashu <krishnavenin8192@gmail.com>
2025-04-26 09:14:56 +0530

    examples of coupling...

--- oreo/src/com/oopsworkz/impl/ACRemote.java
+++ oreo/src/com/oopsworkz/impl/ACRemote.java
@@ -3,6 +3,9 @@ package com.oopsworkz.impl;
 import com.oopsworkz.internal.Remote;
 
 public class ACRemote implements Remote {
+    public ACRemote(){
+        System.out.println("no-arg Constructor of ACRemote");
+    }
     @Override
     public void off() {
         System.out.println("Running off() in ACRemote");
