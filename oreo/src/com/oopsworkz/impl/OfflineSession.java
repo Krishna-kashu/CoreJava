31fa426ae9ae2d5ac5261998738e0a8966490859
Krishna-kashu <krishnavenin8192@gmail.com>
2025-04-27 18:53:01 +0530

    adding constructor to OfflineSession

--- oreo/src/com/oopsworkz/impl/OfflineSession.java
+++ oreo/src/com/oopsworkz/impl/OfflineSession.java
@@ -4,6 +4,9 @@ import com.oopsworkz.internal.Session;
 
 public class OfflineSession implements Session
 {
+    public OfflineSession(){
+        System.out.println("no-arg Constructor of OfflineSession");
+    }
     @Override
     public void attend() {
         System.out.println("Running attend in OfflineClass");
