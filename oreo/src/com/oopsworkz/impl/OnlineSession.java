1a88f16200d78a9ff39f8adfd8d735633e1712ce
Krishna-kashu <krishnavenin8192@gmail.com>
2025-04-27 18:54:41 +0530

    adding constructor to OnlineSession

--- oreo/src/com/oopsworkz/impl/OnlineSession.java
+++ oreo/src/com/oopsworkz/impl/OnlineSession.java
@@ -3,6 +3,9 @@ package com.oopsworkz.impl;
 import com.oopsworkz.internal.Session;
 
 public class OnlineSession implements Session {
+    public OnlineSession(){
+        System.out.println("no-arg constructor of OnlineSession");
+    }
     @Override
     public void attend() {
         System.out.println("Running attend in OnlineClass");
