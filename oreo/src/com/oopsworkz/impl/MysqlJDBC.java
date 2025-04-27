d13f479debb0f194ea9f3262fdd250dda5cded43
Krishna-kashu <krishnavenin8192@gmail.com>
2025-04-27 18:50:57 +0530

    adding constructor to MysqlJDBC

--- oreo/src/com/oopsworkz/impl/MysqlJDBC.java
+++ oreo/src/com/oopsworkz/impl/MysqlJDBC.java
@@ -3,6 +3,9 @@ package com.oopsworkz.impl;
 import com.oopsworkz.internal.JDBC;
 
 public class MysqlJDBC implements JDBC {
+    public MysqlJDBC(){
+        System.out.println("no-arg constructor of MysqlJDBC..");
+    }
     @Override
     public void save() {
         System.out.println("Running save in MysqlJDBC");
