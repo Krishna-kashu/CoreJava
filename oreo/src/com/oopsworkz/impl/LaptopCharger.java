65eab4d189fa30f6bc5ba8e19a24bb1ca4109bd0
Krishna-kashu <krishnavenin8192@gmail.com>
2025-04-27 18:49:14 +0530

    adding constructor to Laptop charger

--- oreo/src/com/oopsworkz/impl/LaptopCharger.java
+++ oreo/src/com/oopsworkz/impl/LaptopCharger.java
@@ -3,6 +3,9 @@ package com.oopsworkz.impl;
 import com.oopsworkz.internal.Charger;
 
 public class LaptopCharger implements Charger {
+    public  LaptopCharger(){
+        System.out.println("no-arg Constructor of Laptop charger");
+    }
     @Override
     public void charge() {
         System.out.println("Running charge in laptopCharger");
