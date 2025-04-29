6952e6672fa6eaf958b92efb62d00d762b4eba43
Krishna-kashu <krishnavenin8192@gmail.com>
2025-04-29 22:32:41 +0530

    working on string operations....

--- stringoperations/src/stringworkz/com/SecondSmallest.java
+++ stringoperations/src/stringworkz/com/SecondSmallest.java
@@ -2,6 +2,19 @@ package stringworkz.com;
 
 public class SecondSmallest {
     public static void main(String[] args) {
+        int[] arr = {2, 8, 9, 3, 1};
+        int smallest =Integer.MAX_VALUE;
+        int secSmallest=Integer.MAX_VALUE;
 
+        for (int num:arr){
+            if(num<smallest){
+                secSmallest=smallest;
+                smallest=num;
+            } else if (num<secSmallest && num !=smallest) {
+                secSmallest=num;
             }
         }
+        System.out.println(smallest);
+        System.out.println(secSmallest);
+    }
+}
