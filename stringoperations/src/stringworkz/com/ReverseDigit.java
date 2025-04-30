00b3a64bf65c1ad8844ae05b7aa378864bc3adfc
Krishna-kashu <krishnavenin8192@gmail.com>
2025-04-30 22:03:04 +0530

    working on string operations....30 april

---
+++ stringoperations/src/stringworkz/com/ReverseDigit.java
@@ -0,0 +1,11 @@
+package stringworkz.com;
+public class ReverseDigit {
+    public static void main(String[] args) {
+        int num = 1234, rev = 0;
+        while(num != 0) {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+        System.out.println("Reversed: " + rev);
+    }
+}
