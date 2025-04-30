24ec0d22094f26f4b8409538d75f93576c51996b
Krishna-kashu <krishnavenin8192@gmail.com>
2025-04-30 22:00:29 +0530

    working on string operations....30th april

---
+++ stringoperations/src/stringworkz/com/DecimalToBinary.java
@@ -0,0 +1,8 @@
+package stringworkz.com;
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        int decimal = 12;
+        String binary = Integer.toBinaryString(decimal);
+        System.out.println("Binary: " + binary);
+    }
+}
