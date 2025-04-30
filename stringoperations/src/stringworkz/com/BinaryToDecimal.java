687ca2d930cd840db7a25b1af9b4df668c890be4
Krishna-kashu <krishnavenin8192@gmail.com>
2025-04-30 21:58:05 +0530

    working on string operations....30 april

---
+++ stringoperations/src/stringworkz/com/BinaryToDecimal.java
@@ -0,0 +1,8 @@
+package stringworkz.com;
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        String binary = "1010";
+        int decimal = Integer.parseInt(binary, 2);
+        System.out.println("Decimal: " + decimal);
+    }
+}
