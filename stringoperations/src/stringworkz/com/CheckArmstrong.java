be5694d665c296ca78b3e1dda8a22c7f40c7ffd5
Krishna-kashu <krishnavenin8192@gmail.com>
2025-05-01 22:26:37 +0530

    working on string operations.... 1st may (2)

---
+++ stringoperations/src/stringworkz/com/CheckArmstrong.java
@@ -0,0 +1,12 @@
+package stringworkz.com;
+public class CheckArmstrong {
+    public static void main(String[] args) {
+        int num = 153, sum = 0, original = num;
+        while(num != 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, 3);
+            num /= 10;
+        }
+        System.out.println("Armstrong: " + (sum == original)); // Output: true
+    }
+}
