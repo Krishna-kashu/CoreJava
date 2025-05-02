bd9abb39eca7519b8445fa277dc262664adc38b2
Krishna-kashu <krishnavenin8192@gmail.com>
2025-05-02 21:28:43 +0530

    working on string operations.... 2nd may (2)

--- stringoperations/src/stringworkz/com/PalindromesInRange.java
+++ stringoperations/src/stringworkz/com/PalindromesInRange.java
@@ -1,6 +1,12 @@
 package stringworkz.com;
 
 public class PalindromesInRange {
+    public static void main(String[] args) {
+        int A = 100, B = 150;
+        for (int i = A; i <= B; i++) {
+            if (isPalindrome(i)) System.out.print(i + " ");
+        }
+    }
     static boolean isPalindrome(int n) {
         int rev = 0, original = n;
         while(n != 0) {
@@ -10,3 +16,4 @@ public class PalindromesInRange {
         return original == rev;
     }
 }
+//101 111 121 131 141
