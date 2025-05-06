7a4b5e91dc459b3447ebc30b2b50a22f12df568c
Krishna-kashu <krishnavenin8192@gmail.com>
2025-05-06 20:04:28 +0530

    working on MySports project using servlet
    index page

--- MySports/src/BadmintonServlet.java
+++ MySports/src/BadmintonServlet.java
@@ -1,2 +1,18 @@
-public class BadmintonServlet {
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/badminton", loadOnStartup = 1)
+public class BadmintonServlet extends GenericServlet {
+
+    public BadmintonServlet(){
+        System.out.println("Badminton Servlet created by tomcat");
+    }
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("Running service badmintonServlet");
+    }
 }
