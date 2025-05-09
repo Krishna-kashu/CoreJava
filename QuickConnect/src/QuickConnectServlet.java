9488f9bf5f7fbd2fbc4e09a400df111dc4f0c625
Krishna-kashu <krishnavenin8192@gmail.com>
2025-05-09 22:19:47 +0530

    working on QuickConnect sevlet project

---
+++ QuickConnect/src/QuickConnectServlet.java
@@ -0,0 +1,14 @@
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/quick", loadOnStartup = 1)
+public class QuickConnectServlet extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+}
