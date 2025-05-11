8d76ba4dfa3495a5ac61c534852ab563bc4cec22
Krishna-kashu <krishnavenin8192@gmail.com>
2025-05-11 17:25:57 +0530

    working on string operations....

--- stringoperations/src/stringworkz/com/CheckPalindrome.java
+++ stringoperations/src/stringworkz/com/CheckPalindrome.java
@@ -1,5 +1,12 @@
 package stringworkz.com;
 
 public class CheckPalindrome {
-
+    public static void main(String[] args) {
+        int num = 121, rev = 0, original = num;
+        while(num != 0) {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+        System.out.println("Is Palindrome: " + (original == rev));
+    }
 }
