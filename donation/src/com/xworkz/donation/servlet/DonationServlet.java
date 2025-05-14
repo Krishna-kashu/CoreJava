6aa07d328313c9b057c482c00358d53d0663b962
Krishna-kashu <krishnavenin8192@gmail.com>
2025-05-14 14:27:11 +0530

    working on QuickConnect sevlet project

---
+++ donation/src/com/xworkz/donation/servlet/DonationServlet.java
@@ -0,0 +1,27 @@
+package com.xworkz.donation.servlet;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/donation", loadOnStartup = 1)
+public class DonationServlet extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        String fname =  servletRequest.getParameter("fname");
+        String lname = servletRequest.getParameter("lname");
+        String mail = servletRequest.getParameter("mail");
+        String donate = servletRequest.getParameter("donation");
+
+        System.out.println("using request dispatcher to forward the req and res to another jsp/servlet");
+        // abstracation : Servlet Chaining
+        RequestDispatcher requestDispatcher=
+                servletRequest.getRequestDispatcher("DonationSuccess.jsp");
+        // jsp--> generate servlet--> service(servletRequest,servletResponse)==>write HTML;
+        requestDispatcher.forward(servletRequest,servletResponse);
+
+
+
+    }
+}
