75d1d50e1567bded0835c6355bda9da187d5657c
Krishna-kashu <krishnavenin8192@gmail.com>
2025-05-19 11:50:39 +0530

    working on servlet Quick forms project FeedBackServlet.java

--- QuickForms/src/com/myproject/quickforms/servlet/FeedBackServlet.java
+++ QuickForms/src/com/myproject/quickforms/servlet/FeedBackServlet.java
@@ -3,23 +3,32 @@ package com.myproject.quickforms.servlet;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/feedback", loadOnStartup = 2)
 public class FeedBackServlet extends HttpServlet {
     @Override
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        System.out.println("Service method called bt Tomcat");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doPost() method called by Tomcat");
 
-        String name = servletRequest.getParameter("name");
-        String number = servletRequest.getParameter("contact");
-        String email = servletRequest.getParameter("email");
-        String feedback = servletRequest.getParameter("feedback");
+        String name = req.getParameter("name");
+        String number = req.getParameter("contact");
+        String email = req.getParameter("email");
+        String feedback = req.getParameter("feedback");
 
-        System.out.println("Using request dispatcher to forward the request and response to another servlet/jsp");
-        RequestDispatcher requestDispatcher = servletRequest.getRequestDispatcher("feedbackSuccess.jsp");
-        requestDispatcher.forward(servletRequest, servletResponse);
-        System.out.println("using request dispatcher to forward the req and res to another jsp/servlet");
+        req.setAttribute("name", name);
+        req.setAttribute("contact", number);
+        req.setAttribute("email", email);
+        req.setAttribute("feedback", feedback);
+        req.setAttribute("message", "Feedback submitted successfully!");
+
+        System.out.println("Forwarding request to feedbackSuccess.jsp");
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("feedbackSuccess.jsp");
+
+        requestDispatcher.forward(req, resp);
+
 
     }
 }
