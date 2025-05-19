aafe91f3caddeccbc45b7cede88b85182d82c3f3
Krishna-kashu <krishnavenin8192@gmail.com>
2025-05-19 11:38:10 +0530

    working on servlet Quick forms project firServlet.java

--- QuickForms/src/com/myproject/quickforms/servlet/FIRServlet.java
+++ QuickForms/src/com/myproject/quickforms/servlet/FIRServlet.java
@@ -3,23 +3,32 @@ package com.myproject.quickforms.servlet;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/fir", loadOnStartup = 3)
 public class FIRServlet extends HttpServlet {
     @Override
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        System.out.println("Service method called bt Tomcat");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doPost() method called by Tomcat");
 
-        String name = servletRequest.getParameter("name");
-        String number = servletRequest.getParameter("contact");
-        String datetime = servletRequest.getParameter("datetime");
-        String location = servletRequest.getParameter("location");
-        String description = servletRequest.getParameter("description");
+        String name = req.getParameter("name");
+        String number = req.getParameter("contact");
+        String datetime = req.getParameter("datetime");
+        String location = req.getParameter("location");
+        String description = req.getParameter("description");
 
-        System.out.println("Using request dispatcher to forward the request and response to another servlet/jsp");
-        RequestDispatcher requestDispatcher = servletRequest.getRequestDispatcher("firSuccess.jsp");
-        requestDispatcher.forward(servletRequest, servletResponse);
-        System.out.println("using request dispatcher to forward the req and res to another jsp/servlet");
+        req.setAttribute("message", "Save Success");
+        req.setAttribute("name", name);
+        req.setAttribute("contact", number);
+        req.setAttribute("datetime", datetime);
+        req.setAttribute("location", location);
+        req.setAttribute("description", description);
+
+        System.out.println("Forwarding request to firSuccess.jsp");
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("firSuccess.jsp");
+        requestDispatcher.forward(req, resp);
+        System.out.println("using request dispatcher to forward the req and resp to another jsp/servlet");
     }
     }
