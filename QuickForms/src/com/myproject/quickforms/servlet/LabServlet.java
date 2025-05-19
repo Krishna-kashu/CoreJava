038459fb16d62cdf5849c15259b744c82849c3ab
Krishna-kashu <krishnavenin8192@gmail.com>
2025-05-19 11:50:49 +0530

    working on servlet Quick forms project LabServlet.java

--- QuickForms/src/com/myproject/quickforms/servlet/LabServlet.java
+++ QuickForms/src/com/myproject/quickforms/servlet/LabServlet.java
@@ -3,22 +3,31 @@ package com.myproject.quickforms.servlet;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/lab", loadOnStartup = 4)
 public class LabServlet extends HttpServlet {
     @Override
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        System.out.println("Service method called bt Tomcat");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doPost() method called by Tomcat");
 
-        String name = servletRequest.getParameter("name");
-        String number = servletRequest.getParameter("RollNumber");
-        String RNumber = servletRequest.getParameter("RNumber");
-        String observation = servletRequest.getParameter("observation");
+        String name = req.getParameter("name");
+        String number = req.getParameter("RollNumber");
+        String RNumber = req.getParameter("RNumber");
+        String observation = req.getParameter("observation");
 
         System.out.println("Using request dispatcher to forward the request and response to another servlet/jsp");
-        RequestDispatcher requestDispatcher = servletRequest.getRequestDispatcher("laboratorySuccess.jsp");
-        requestDispatcher.forward(servletRequest, servletResponse);
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("laboratorySuccess.jsp");
+
+        req.setAttribute("name", name);
+        req.setAttribute("RollNumber", number);
+        req.setAttribute("RNumber", RNumber);
+        req.setAttribute("observation", observation);
+        req.setAttribute("message", "Observation recorded successfully!");
+
+        requestDispatcher.forward(req, resp);
         System.out.println("using request dispatcher to forward the req and res to another jsp/servlet");
     }
     }
