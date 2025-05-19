0f2daa2b5b883dde7efb453d07db6e290f3eb866
Krishna-kashu <krishnavenin8192@gmail.com>
2025-05-19 11:50:25 +0530

    working on servlet Quick forms project DonationServlet.java

--- QuickForms/src/com/myproject/quickforms/servlet/DonationServlet.java
+++ QuickForms/src/com/myproject/quickforms/servlet/DonationServlet.java
@@ -3,23 +3,31 @@ package com.myproject.quickforms.servlet;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/donate", loadOnStartup = 1)
 public class DonationServlet extends HttpServlet {
     @Override
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        System.out.println("Service method called bt Tomcat");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doPost() method called by Tomcat");
 
-        String name = servletRequest.getParameter("donorName");
-        String number = servletRequest.getParameter("donationAmount");
-        String email = servletRequest.getParameter("donorEmail");
-        String message = servletRequest.getParameter("donationMessage");
+    String name = req.getParameter("donorName");
+        String number = req.getParameter("donationAmount");
+        String email = req.getParameter("donorEmail");
+        String message = req.getParameter("donationMessage");
 
         System.out.println("Using request dispatcher to forward the request and response to another servlet/jsp");
-        RequestDispatcher requestDispatcher = servletRequest.getRequestDispatcher("donationSuccess.jsp");
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("donationSuccess.jsp");
 
-        requestDispatcher.forward(servletRequest,servletResponse);
+        req.setAttribute("donorName", name);
+        req.setAttribute("donationAmount", number);
+        req.setAttribute("donorEmail", email);
+        req.setAttribute("donationMessage", message);
+        req.setAttribute("message", "Donation received successfully!");
+
+        requestDispatcher.forward(req,resp);
         System.out.println("using request dispatcher to forward the req and res to another jsp/servlet");
 
     }
