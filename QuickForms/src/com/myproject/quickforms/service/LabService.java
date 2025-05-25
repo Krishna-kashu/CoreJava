031e8e2e94d1a4b4314d937a5d92f2ab6e708fcf
Krishna-kashu <krishnavenin8192@gmail.com>
2025-05-25 18:09:12 +0530

    working on servlet Quick forms project

---
+++ QuickForms/src/com/myproject/quickforms/service/LabService.java
@@ -0,0 +1,7 @@
+package com.myproject.quickforms.service;
+
+import com.myproject.quickforms.dto.LabDTO;
+
+public interface LabService {
+    public boolean save(LabDTO labDTO);
+}
---
+++ QuickForms/src/com/myproject/quickforms/service/LabServiceImpl.java
@@ -0,0 +1,10 @@
+package com.myproject.quickforms.service;
+
+import com.myproject.quickforms.dto.LabDTO;
+
+public class LabServiceImpl implements LabService{
+    @Override
+    public boolean save(LabDTO labDTO) {
+        return true;
+    }
+}
