5fb4eb65affffd8a99c44663819bba45d69f09a8
Krishna-kashu <krishnavenin8192@gmail.com>
2025-05-26 21:19:35 +0530

    working on servlet Quick forms project

--- QuickForms/src/com/myproject/quickforms/dto/DonationDTO.java
+++ QuickForms/src/com/myproject/quickforms/dto/DonationDTO.java
@@ -8,43 +8,43 @@ public class DonationDTO {
     private String amount;
     private String description;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setAmount(String amount) {
-        this.amount = amount;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getContact() {
         return contact;
     }
 
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getAmount() {
         return amount;
     }
 
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
