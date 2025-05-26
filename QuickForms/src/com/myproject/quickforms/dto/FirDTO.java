d7628d0f4f862cb37bc87b3319acbf631ab3b507
Krishna-kashu <krishnavenin8192@gmail.com>
2025-05-26 21:20:16 +0530

    working on servlet Quick forms project

--- QuickForms/src/com/myproject/quickforms/dto/FirDTO.java
+++ QuickForms/src/com/myproject/quickforms/dto/FirDTO.java
@@ -7,41 +7,40 @@ public class FirDTO {
     private String location;
     private String description;
 
-
     public String getName() {
         return name;
-    }
-
-    public String getContact() {
-        return contact;
-    }
-
-    public String getDatetime() {
-        return datetime;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getContact() {
+        return contact;
+    }
+
     public void setContact(String contact) {
         this.contact = contact;
+    }
+
+    public String getDatetime() {
+        return datetime;
     }
 
     public void setDatetime(String datetime) {
         this.datetime = datetime;
     }
 
+    public String getLocation() {
+        return location;
+    }
+
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public void setDescription(String description) {
