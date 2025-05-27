01e5f42c7e3e5602fa719dc9d8ad9e44b7c1161a
Krishna-kashu <krishnavenin8192@gmail.com>
2025-05-27 22:21:56 +0530

    working on servlet project EventMate

--- EventMate/src/EventServlet.java
+++ EventMate/src/EventServlet.java
@@ -2,9 +2,10 @@ import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
-
+@WebServlet(urlPatterns = "/event", loadOnStartup = 1)
 public class EventServlet extends GenericServlet {
 
     @Override
