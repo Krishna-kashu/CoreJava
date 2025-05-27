e0377f5dec642256481c1bedc3e81d2f259b3b7f
Krishna-kashu <krishnavenin8192@gmail.com>
2025-05-27 22:21:28 +0530

    working on servlet project EventMate

--- EventMate/src/EventServlet.java
+++ EventMate/src/EventServlet.java
@@ -1,2 +1,14 @@
-public class EventServlet {
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+
+public class EventServlet extends GenericServlet {
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("Running Service by tomcat");
+    }
 }
