957fc3dd7c22d9533b8671621ce0c3a566ec97d9
Krishna-kashu <krishnavenin8192@gmail.com>
2025-06-02 22:42:13 +0530

    working on servlet project EventMate

--- EventMate/src/dto/EventDTO.java
+++ EventMate/src/dto/EventDTO.java
@@ -1,9 +1,11 @@
 package dto;
 
 public class EventDTO {
+
     public EventDTO(){
         System.out.println("no-arg constructor of EventDTO");
     }
+    
     @Override
     public String toString() {
         return "EventDTO{}";
