bb163d9b5f1f77af381c8a8c038b2524e6ce0480
Krishna-kashu <krishnavenin8192@gmail.com>
2025-06-02 22:41:02 +0530

    working on servlet project EventMate

--- EventMate/src/service/EventServiceImpl.java
+++ EventMate/src/service/EventServiceImpl.java
@@ -3,9 +3,11 @@ package service;
 import dto.EventDTO;
 
 public class EventServiceImpl implements EventService{
+    
     public EventServiceImpl(){
         System.out.println("no-arg constructor of EventServiceImpl");
     }
+
     @Override
     public boolean save(EventDTO eventDTO) {
         return false;
