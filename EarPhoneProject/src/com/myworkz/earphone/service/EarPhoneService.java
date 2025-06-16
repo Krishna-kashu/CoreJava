16ad0bed93d377621ba9fce54eb97c312757da62
Krishna-kashu <krishnavenin8192@gmail.com>
2025-06-16 22:37:16 +0530

    working on Servlet and Service Integration

--- EarPhoneProject/src/com/myworkz/earphone/service/EarPhoneService.java
+++ EarPhoneProject/src/com/myworkz/earphone/service/EarPhoneService.java
@@ -2,6 +2,12 @@ package com.myworkz.earphone.service;
 
 import com.myworkz.earphone.dto.EarPhoneDTO;
 
+import java.util.Optional;
+
 public interface EarPhoneService {
     boolean save(EarPhoneDTO earPhoneDTO);
+
+    default Optional<EarPhoneDTO> findById(int id){
+        return Optional.empty();
+    }
 }
