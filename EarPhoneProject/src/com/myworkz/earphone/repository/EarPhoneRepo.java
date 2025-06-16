4c39a8987b25b01390c36a5e6524c79b2e91cd4d
Krishna-kashu <krishnavenin8192@gmail.com>
2025-06-16 22:35:44 +0530

    working on JDBC

--- EarPhoneProject/src/com/myworkz/earphone/repository/EarPhoneRepo.java
+++ EarPhoneProject/src/com/myworkz/earphone/repository/EarPhoneRepo.java
@@ -2,6 +2,12 @@ package com.myworkz.earphone.repository;
 
 import com.myworkz.earphone.dto.EarPhoneDTO;
 
+import java.util.Optional;
+
 public interface EarPhoneRepo {
     boolean persist(EarPhoneDTO earPhoneDTO);
+
+    default Optional<EarPhoneDTO> findById(int id){
+        return Optional.empty();
+    }
 }
