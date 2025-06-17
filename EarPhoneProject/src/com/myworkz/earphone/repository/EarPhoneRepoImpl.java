67ee8bf12c831f0e187ac4e915ce0ae80ce3c505
Krishna-kashu <krishnavenin8192@gmail.com>
2025-06-17 22:40:24 +0530

    working on Database Connection

--- EarPhoneProject/src/com/myworkz/earphone/repository/EarPhoneRepoImpl.java
+++ EarPhoneProject/src/com/myworkz/earphone/repository/EarPhoneRepoImpl.java
@@ -1,9 +1,10 @@
 package com.myworkz.earphone.repository;
 
-import com.mysql.cj.jdbc.Driver;
+import com.myworkz.earphone.constant.DBProperties;
 import com.myworkz.earphone.dto.EarPhoneDTO;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.Optional;
 
 public class EarPhoneRepoImpl implements EarPhoneRepo {
@@ -12,25 +13,24 @@ public class EarPhoneRepoImpl implements EarPhoneRepo {
         System.out.println("running persist in EarPhoneRepoImpl");
         if (earPhoneDTO != null) {
             System.out.println("earPhoneDTO is not null");
-            String url = "jdbc:mysql://localhost:3306/book_worm";
-            String userName = "root";
-            String password = "Krishna@2002";
+
 
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
 
-                Connection connection = DriverManager.getConnection(url, userName, password);
+                Connection connection = DriverManager.getConnection(DBProperties.URL.getProp(), DBProperties.USER_NAME.getProp(), DBProperties.SECRET.getProp());
 
                 String sql = "INSERT INTO earphone_registration_table (brand, type, cost, features, color, warranty) VALUES (?, ?, ?, ?, ?, ?)";
-                PreparedStatement stmt = connection.prepareStatement(sql);
-                stmt.setString(1, earPhoneDTO.getBrand());
-                stmt.setString(2, earPhoneDTO.getType());
-                stmt.setDouble(3, earPhoneDTO.getCost());
-                stmt.setString(4, earPhoneDTO.getFeatures());
-                stmt.setString(5, earPhoneDTO.getColor());
-                stmt.setString(6, earPhoneDTO.getWarranty());
+                PreparedStatement statement = connection.prepareStatement(sql);
+                statement.setString(1, earPhoneDTO.getBrand());
+                statement.setString(2, earPhoneDTO.getType());
+                statement.setDouble(3, earPhoneDTO.getCost());
+                statement.setString(4, earPhoneDTO.getFeatures());
+                statement.setString(5, earPhoneDTO.getColor());
+                statement.setString(6, earPhoneDTO.getWarranty());
 
-                stmt.executeUpdate();
+                statement.executeUpdate(sql);
+
 
             } catch (ClassNotFoundException | SQLException e) {
                 throw new RuntimeException(e);
@@ -45,6 +45,37 @@ public class EarPhoneRepoImpl implements EarPhoneRepo {
     @Override
     public Optional<EarPhoneDTO> findById(int id) {
         System.out.println("Running overridden findById in EarPhoneRepoImpl");
-        return EarPhoneRepo.super.findById(id);
+        //JDBC
+        // Load & register , Create the Connection , Prepare the Statement , Execute the statement
+        //URL,UN,PWD
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(DBProperties.URL.getProp(), DBProperties.USER_NAME.getProp(), DBProperties.SECRET.getProp());
+            String findByIdSQL="select * from earphone_registration_table earPhone where earPhone.id="+id+"";
+            System.out.println("findById :"+findByIdSQL);
+            Statement statement=connection.createStatement();
+            ResultSet resultSet= statement.executeQuery(findByIdSQL);
+            System.out.println("resultSet :"+resultSet);
+            while(resultSet.next())
+            {
+                int pk= resultSet.getInt("id");
+                String brand =resultSet.getString("brand");
+                String type =resultSet.getString("type");
+                int cost=resultSet.getInt("cost");
+                String features =resultSet.getString("features");
+                String color=resultSet.getString("color");
+                String warranty =resultSet.getString("warranty");
+//                LocalDate created_at = resultSet.getDate("created_at").toLocalDate();
+
+                EarPhoneDTO earPhoneDTO =new EarPhoneDTO(brand, type,cost, features,color, warranty);
+                return  Optional.of(earPhoneDTO);
+            }
+
+        }
+        catch (SQLException | ClassNotFoundException exception)
+        {
+            System.err.println("SQLException in findById "+exception.getMessage());
+        }
+        return Optional.empty();
     }
 }
