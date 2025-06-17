2abbdb32471a97890191207f42cc12419192b0af
Krishna-kashu <krishnavenin8192@gmail.com>
2025-06-17 23:05:40 +0530

    working on Database Connection

--- EarPhoneProject/src/com/myworkz/earphone/servlet/EarPhoneServlet.java
+++ EarPhoneProject/src/com/myworkz/earphone/servlet/EarPhoneServlet.java
@@ -98,7 +98,7 @@ public class EarPhoneServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Running doDet method in");
+        System.out.println("Running doDet method in Servlet");
         String earPhoneID = req.getParameter("phoneID");
 
         if(earPhoneID!=null){
