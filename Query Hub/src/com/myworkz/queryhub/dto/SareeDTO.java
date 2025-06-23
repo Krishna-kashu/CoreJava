d5ef3e131c0d05abfd11da4280da29273dc413aa
Krishna-kashu <krishnavenin8192@gmail.com>
2025-06-23 22:19:02 +0530

    working on jdbc

---
+++ Query Hub/src/com/myworkz/queryhub/dto/SareeDTO.java
@@ -0,0 +1,35 @@
+package com.myworkz.queryhub.dto;
+
+
+/* Create a Collection of SareeDTOs (type,color,material,cost) with 10 elements
+Loop it and print it on Console
+
+Create a Collection of Camping Accessories (5 properties) with 10 elements
+Loop it and print it on Console
+
+Create a Collection of furniture  (5 properties) with 10 elements
+Loop it and print it on Console
+
+Create a Collection of appliances  (5 properties) with 10 elements
+Loop it and print it on Console
+
+Create a Collection of your choice  (5 properties) with 10 elements
+Loop it and print it on Console
+*/
+
+
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class SareeDTO {
+    private String material;
+    private String name;
+    private double cost;
+    private String pattern;
+    private String color;
+    private String occasion;
+}
