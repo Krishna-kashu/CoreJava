7e653a32525b45046bfb9ea4857203af2612a629
Krishna-kashu <krishnavenin8192@gmail.com>
2025-06-30 22:41:27 +0530

    working on Collection

--- Query Hub/src/com/myworkz/queryhub/task/CollectionTask.java
+++ Query Hub/src/com/myworkz/queryhub/task/CollectionTask.java
@@ -1,8 +1,6 @@
 package com.myworkz.queryhub.task;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedList;
+import java.util.*;
 
 public class CollectionTask {
     public static void main(String[] args) {
@@ -18,9 +16,30 @@ public class CollectionTask {
         cities.add("Hasan");
         cities.add("Davanagere");
 
+        System.out.println(cities);
         for(String city: cities){
             System.out.println(city);
         }
+
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(10, 60, 30, 90, 25));
+        Iterator<Integer> iterator = numbers.iterator();
+
+        System.out.println(numbers);
+        while (iterator.hasNext()) {
+            if (iterator.next() > 50) iterator.remove();
+        }
+        System.out.println("After removing > 50: " + numbers);
+
+        HashSet<String> fruits = new HashSet<>();
+        fruits.add("Apple");
+        fruits.add("Mango");
+        fruits.add("Banana");
+        fruits.add("Grapes");
+        fruits.add("Apple");
+        System.out.println(" Fruits set: " + fruits);
+
+        List<String> lang = Arrays.asList("Python", "JavaScript", "Java");
+        System.out.println(" Contains 'Java'? " + lang.contains("Java"));
 
     }
 }
