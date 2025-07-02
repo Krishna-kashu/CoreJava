f25efda63b95225a438f03621ecad0b97acf2973
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-02 21:56:27 +0530

    working on Collection

--- Query Hub/src/com/myworkz/queryhub/dto/SareeDTO.java
+++ Query Hub/src/com/myworkz/queryhub/dto/SareeDTO.java
@@ -8,11 +8,25 @@ import lombok.*;
 @Setter
 @ToString
 @NoArgsConstructor
-public class SareeDTO {
+public class SareeDTO implements Comparable<SareeDTO>{
     private String material;
     private String name;
     private double cost;
     private String pattern;
     private String color;
     private String occasion;
+
+
+    @Override
+    public int compareTo(SareeDTO o) {
+        System.out.println("Running compareTo");
+        SareeDTO leftSide = this;
+//        SareeDTO rightSide = o;
+        if(leftSide.cost >o.cost){
+            return  -6;
+        }else if(leftSide.cost < o.cost){
+            return 5;
+        }
+        return 0;
+    }
 }
