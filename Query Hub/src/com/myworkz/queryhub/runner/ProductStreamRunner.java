cfa31068e1071656189223478cad8b4e6a12256e
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-02 22:56:18 +0530

    working on Collection and Streams

--- Query Hub/src/com/myworkz/queryhub/runner/ProductStreamRunner.java
+++ Query Hub/src/com/myworkz/queryhub/runner/ProductStreamRunner.java
@@ -11,7 +11,17 @@ public class ProductStreamRunner {
         ProductRepo productRepo = new ProductRepoImpl();
         List<ProductDTO> list = productRepo.findAll();
 
-        System.out.println("******** All products ********");
+        System.out.println("\n******** All products ********");
         list.forEach(System.out::println);
+
+        System.out.println("\n\n all name's of the product");
+        list.stream().map(ProductDTO::getName).forEach(System.out::println);
+
+        System.out.println("\n\n all product where discount is greater than 10 %");
+        list.stream().filter(a -> a.getDiscount()>10).forEach(System.out::println);
+
+        System.out.println("");
+
+
     }
 }
