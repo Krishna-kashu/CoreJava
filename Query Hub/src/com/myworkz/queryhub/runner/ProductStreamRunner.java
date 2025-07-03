1f87614e5818d5d367dfc675d773046e42c7bf91
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-03 10:58:33 +0530

    working on Collection and Streams

--- Query Hub/src/com/myworkz/queryhub/runner/ProductStreamRunner.java
+++ Query Hub/src/com/myworkz/queryhub/runner/ProductStreamRunner.java
@@ -4,7 +4,10 @@ import com.myworkz.queryhub.dto.ProductDTO;
 import com.myworkz.queryhub.repository.ProductRepo;
 import com.myworkz.queryhub.repository.ProductRepoImpl;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class ProductStreamRunner {
     public static void main(String[] args) {
@@ -20,7 +23,13 @@ public class ProductStreamRunner {
         System.out.println("\n\n all product where discount is greater than 10 %");
         list.stream().filter(a -> a.getDiscount()>10).forEach(System.out::println);
 
-        System.out.println("");
+        System.out.println("\n\n all product if warranty less than 3");
+        List<ProductDTO> collect = list.stream().filter(e -> e.getWarranty() < 3).collect(Collectors.toList());
+        collect.forEach(System.out::println);
+
+        System.out.println("\n\n all product by id asc");
+        list.stream().sorted().forEach(System.out::println);
+
 
 
     }
