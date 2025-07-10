0dd5644837c2525bd980f63fc8698291b391c1c8
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-10 18:52:09 +0530

    working on maps

--- maps/src/com/my_work/mapps/dto/PersonDTO.java
+++ maps/src/com/my_work/mapps/dto/PersonDTO.java
@@ -1,12 +1,34 @@
 package com.my_work.mapps.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
-@Data
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class PersonDTO {
     private String name;
     private String type;
     private String aadhaar;
+
+    @Override
+    public int hashCode() {
+        System.out.println("running hashcode...");
+        return Objects.hashCode(aadhaar);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        System.out.println("running equals...");
+        if(obj!=null){
+            if(obj instanceof PersonDTO){
+                PersonDTO person =(PersonDTO) obj;
+                return this.name.equalsIgnoreCase(person.name);
+            }
+        }
+        return false;
+    }
 }
