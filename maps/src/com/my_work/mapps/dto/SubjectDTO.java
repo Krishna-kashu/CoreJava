17f296e7dd6bdc4b56beab4d1076fa240e383b56
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-12 22:38:47 +0530

    working on set and sorting

---
+++ maps/src/com/my_work/mapps/dto/SubjectDTO.java
@@ -0,0 +1,13 @@
+package com.my_work.mapps.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class SubjectDTO {
+    private String name;
+    private String code;
+}
