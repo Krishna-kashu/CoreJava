16bbe72297dd5889f6a4c45c2a841fde30b9cc42
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-13 09:10:03 +0530

    working on set and sorting

---
+++ maps/src/com/my_work/mapps/dto/MarksDTO.java
@@ -0,0 +1,13 @@
+package com.my_work.mapps.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MarksDTO {
+    private int marksObtained;
+    private int totalMarks;
+}
