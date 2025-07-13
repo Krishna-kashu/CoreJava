1b36229454581398e91362b2f5b844fa751cf784
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-13 09:10:25 +0530

    working on set and sorting

---
+++ maps/src/com/my_work/mapps/dto/VendorDTO.java
@@ -0,0 +1,14 @@
+package com.my_work.mapps.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class VendorDTO {
+    private String name;
+    private String gstNo;
+    private String establishedDate;
+}
