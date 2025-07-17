3a8eab68ae00eda72b884e3de726b846b9ce7c56
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-17 22:35:10 +0530

    working on spring

---
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/component/OilComponent.java
@@ -0,0 +1,10 @@
+package com.mywork.elasticspring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OilComponent {
+    public OilComponent() {
+        System.out.println("Created OilComponent...");
+    }
+}
