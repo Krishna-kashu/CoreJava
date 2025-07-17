5b5b9c7304b9e9555eaf2fdc7e08c3888cb69bbb
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-17 22:34:19 +0530

    working on spring

---
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/component/AgricultureComponent.java
@@ -0,0 +1,10 @@
+package com.mywork.elasticspring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AgricultureComponent {
+    public AgricultureComponent(){
+        System.out.println("created AgricultureComponent");
+    }
+}
