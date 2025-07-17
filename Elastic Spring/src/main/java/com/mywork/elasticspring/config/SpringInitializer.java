d90eacb4f7bebcb005caa5a3386e2108df7d8d84
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-17 22:34:42 +0530

    working on spring

--- Elastic Spring/src/main/java/com/mywork/elasticspring/config/SpringInitializer.java
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/config/SpringInitializer.java
@@ -1,2 +1,27 @@
-package com.mywork.elasticspring.config;public class SpringInitializer {
+package com.mywork.elasticspring.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    public SpringInitializer(){
+        System.out.println("created SpringInitializer...");
+    }
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        System.out.println("running getRootConfigClasses...");
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        System.out.println("running getServletConfigClasses...");
+        return new Class[]{SpringConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        System.out.println("running getServletMappings...");
+        return new String[0];
+}
 }
