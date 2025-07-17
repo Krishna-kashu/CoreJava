6a69f7fd12f0ca42ef7b54ad13fcca7e71d33e21
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-17 22:35:51 +0530

    working on spring

---
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/component/CosmeticComponent.java
@@ -0,0 +1,10 @@
+package com.mywork.elasticspring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CosmeticComponent {
+    public CosmeticComponent() {
+        System.out.println("Created  CosmeticComponent....." );
+    }
+}
