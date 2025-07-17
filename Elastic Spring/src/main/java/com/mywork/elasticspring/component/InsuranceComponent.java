2ce128be48549c19a0cd5d63bb339ad814f4bdde
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-17 22:35:31 +0530

    working on spring

---
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/component/InsuranceComponent.java
@@ -0,0 +1,10 @@
+package com.mywork.elasticspring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class InsuranceComponent {
+    public InsuranceComponent() {
+        System.out.println("Created   InsuranceComponent  ....." );
+    }
+}
