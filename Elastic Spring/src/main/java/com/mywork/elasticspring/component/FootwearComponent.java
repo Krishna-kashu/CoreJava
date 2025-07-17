e7d0844ce274f6c8ab69b88f782e3b766c592beb
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-17 22:35:25 +0530

    working on spring

---
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/component/FootwearComponent.java
@@ -0,0 +1,10 @@
+package com.mywork.elasticspring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FootwearComponent {
+    public FootwearComponent() {
+        System.out.println("Created    FootwearComponent ....." );
+    }
+}
