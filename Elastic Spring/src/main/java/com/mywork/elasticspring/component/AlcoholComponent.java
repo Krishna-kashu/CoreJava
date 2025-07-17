953851aace6382ecd9cef384b0f578b318a443d1
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-17 22:34:23 +0530

    working on spring

---
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/component/AlcoholComponent.java
@@ -0,0 +1,11 @@
+package com.mywork.elasticspring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AlcoholComponent
+{
+    public AlcoholComponent(){
+        System.out.println("created AlcoholComponent");
+    }
+}
