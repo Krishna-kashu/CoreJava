6bb1ad1bdb14493ab1a14efc1c78fb83c9b582bc
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-18 22:26:32 +0530

    working on spring

---
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/component/CoconutComponent.java
@@ -0,0 +1,10 @@
+package com.mywork.elasticspring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CoconutComponent {
+    public CoconutComponent() {
+        System.out.println("created  CoconutComponent.....");
+    }
+}
