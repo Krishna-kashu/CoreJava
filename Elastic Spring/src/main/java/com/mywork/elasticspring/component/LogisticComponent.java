e5a586ba7f5d53bb7822e8abfdad95e92eb12721
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-19 10:55:21 +0530

    working on spring initialization and configuration

---
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/component/LogisticComponent.java
@@ -0,0 +1,10 @@
+package com.mywork.elasticspring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LogisticComponent {
+    public LogisticComponent() {
+        System.out.println("Created LogisticComponent...");
+    }
+}
