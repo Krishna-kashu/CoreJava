f07d14b774c5185d819e4548d6f8a3a3599a39bd
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-19 10:55:17 +0530

    working on spring initialization and configuration

---
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/component/MovieComponent.java
@@ -0,0 +1,11 @@
+package com.mywork.elasticspring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MovieComponent
+{
+    public MovieComponent() {
+        System.out.println("Created MovieComponent...");
+    }
+}
