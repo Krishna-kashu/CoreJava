82ae3d1daa89ab8f16e2e0d83871bea8238bae58
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-19 10:55:07 +0530

    working on spring initialization and configuration

---
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/component/HospitalComponent.java
@@ -0,0 +1,10 @@
+package com.mywork.elasticspring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HospitalComponent {
+    public HospitalComponent() {
+        System.out.println("Created   HospitalComponent  ....." );
+    }
+}
