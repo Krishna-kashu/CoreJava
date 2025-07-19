e21ab8eda6350ae8953c146e7825ddb00469808c
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-19 10:55:12 +0530

    working on spring initialization and configuration

---
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/component/ManufacturingComponent.java
@@ -0,0 +1,11 @@
+package com.mywork.elasticspring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ManufacturingComponent
+{
+    public ManufacturingComponent() {
+        System.out.println("Created ManufacturingComponent...");
+    }
+}
