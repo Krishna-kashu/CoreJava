31ceff113810516200c079e5d2ae2220ff3c7343
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-19 10:55:03 +0530

    working on spring initialization and configuration

---
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/component/HotelComponent.java
@@ -0,0 +1,11 @@
+package com.mywork.elasticspring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HotelComponent
+{
+    public HotelComponent(){
+        System.out.println("Created   HotelComponent  ....." );
+    }
+}
