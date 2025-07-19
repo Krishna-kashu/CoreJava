f1f5d5a162bfb0a8233fe738550b03d423952057
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-19 10:55:36 +0530

    working on spring initialization and configuration

---
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/component/SpicesComponent.java
@@ -0,0 +1,10 @@
+package com.mywork.elasticspring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpicesComponent {
+    public SpicesComponent() {
+        System.out.println("Created SpicesComponent...");
+    }
+}
