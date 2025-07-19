943f0eac4cf8c6982c97c0f5fd50270375e8ba56
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-19 10:55:26 +0530

    working on spring initialization and configuration

---
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/component/RealEstateComponent.java
@@ -0,0 +1,10 @@
+package com.mywork.elasticspring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RealEstateComponent {
+    public RealEstateComponent() {
+        System.out.println("Created RealEstateComponent...");
+    }
+}
