4a6f95956665a7bd909ac5cdbe5d4fdbf5cc2f56
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-20 10:37:25 +0530

    working on spring initialization and configuration

---
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/component/TravelComponent.java
@@ -0,0 +1,11 @@
+package com.mywork.elasticspring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TravelComponent
+{
+    public TravelComponent() {
+        System.out.println("Created TravelComponent...");
+    }
+}
