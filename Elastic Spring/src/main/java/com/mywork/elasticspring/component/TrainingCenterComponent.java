12ec15e898e447a8206f65e611ee40fd0e25cc43
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-20 10:37:20 +0530

    working on spring initialization and configuration

---
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/component/TrainingCenterComponent.java
@@ -0,0 +1,11 @@
+package com.mywork.elasticspring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrainingCenterComponent
+{
+    public TrainingCenterComponent() {
+        System.out.println("Created TrainingCenterComponent...");
+    }
+}
