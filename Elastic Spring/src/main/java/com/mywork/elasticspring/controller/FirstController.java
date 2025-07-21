8c4e643fac3087151bf87e0fcc9cb2c101ada20b
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-21 22:16:04 +0530

    working on spring initialization and configuration

--- Elastic Spring/src/main/java/com/mywork/elasticspring/controller/FirstController.java
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/controller/FirstController.java
@@ -1,12 +1,15 @@
 package com.mywork.elasticspring.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class FirstController {
     public FirstController(){
         System.out.println("Calling first controller by spring container(ApplicationContext)");
     }
 
     public String onSend(){
-
+        System.out.println("running onSend after clicking send");
         return "/index.jsp";
     }
 }
