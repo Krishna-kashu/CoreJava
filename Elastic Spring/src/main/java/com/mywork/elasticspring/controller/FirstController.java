25aafd56fc7bb5974cef2e0d7d8870bcb42df7df
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-22 15:52:31 +0530

    working on spring initialization and configuration

--- Elastic Spring/src/main/java/com/mywork/elasticspring/controller/FirstController.java
+++ Elastic Spring/src/main/java/com/mywork/elasticspring/controller/FirstController.java
@@ -5,15 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
-public class FirstController {
-    public FirstController(){
-        System.out.println("Calling first controller by spring container(ApplicationContext)");
-    }
 
-    @RequestMapping(value = "/send")
-    public String onSend(){
-        System.out.println("running onSend after clicking send");
-        return "/WEB-INF/index.jsp";
+public class FirstController {
+
+    @RequestMapping("/")
+    public String load(){
+        System.out.println("loading index");
+        return "index";
+    }
+    @RequestMapping(value = "/send", method = RequestMethod.POST)
+    public String onSend() {
+        System.out.println("Send button clicked");
+        return "index";
     }
 }
