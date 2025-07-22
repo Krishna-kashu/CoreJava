be232b224fd431ca7565b770ed4d97bce81c18ff
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-22 21:34:45 +0530

    Spring MVC web app with JSP and form handling using Maven

---
+++ green-dress/src/main/java/com/myworks/greendress/controller/FirstController.java
@@ -0,0 +1,19 @@
+package com.myworks.greendress.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class FirstController {
+
+    public FirstController(){
+        System.out.println("no arg constructor of FirstController");
+    }
+    @RequestMapping("/send")
+    public String onSend() {
+        System.out.println("Send button clicked");
+        return "index.jsp";
+    }
+
+}
