031ab0bb5577bed88b20689c71707107d1ad34e8
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-23 18:22:01 +0530

    Spring MVC web app with JSP and form handling using Maven

---
+++ green-dress/src/main/java/com/myworks/greendress/controller/RTOController.java
@@ -0,0 +1,28 @@
+package com.myworks.greendress.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class RTOController {
+    public RTOController(){
+        System.out.println("RTO controller");
+    }
+    @RequestMapping("/submitRTO")
+    public String onSubmit(@RequestParam String vehicleNumber, @RequestParam String ownerName, @RequestParam String vehicleType, @RequestParam String registrationDate, @RequestParam String state, @RequestParam String contact, Model model){
+        System.out.println("onSubmit RTOController");
+
+        model.addAttribute("vehicleNumber",vehicleNumber);
+        model.addAttribute("ownerName", ownerName);
+        model.addAttribute("vehicleType", vehicleType);
+        model.addAttribute("registrationDate", registrationDate);
+        model.addAttribute("state", state);
+        model.addAttribute("contact", contact);
+
+        System.out.println("vehicleNumber: " +vehicleNumber+ ", ownerName: " + ownerName + ", vehicleType: " + vehicleType + ", registrationDate: " + registrationDate + ", state: " + state + ", contact: " + contact);
+        return "result.jsp";
+    }
+}
