3df773efb9302b0f84130865594e11d70330532c
Krishna-kashu <krishnavenin8192@gmail.com>
2025-07-24 18:41:20 +0530

    Spring MVC web app with JSP and form handling using Maven

---
+++ green-dress/src/main/java/com/myworks/greendress/dto/FruitDTO.java
@@ -0,0 +1,20 @@
+package com.myworks.greendress.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FruitDTO {
+    private String fruitName;
+    private String color;
+    private String taste;
+    private String season;
+    private String price;
+    private String origin;
+    private String vitamin;
+    private String seed;
+    private String life;
+    private String dish;
+}
